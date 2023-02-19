928189cb805aaeaa4a6cdd032453f0fbb0b1d750
namaridamaX <b2200170@photon.chitose.ac.jp>
2023-02-19 21:27:15 +0900

    beautifulsoupを用いたPDFの取得に成功

--- app/connection_web.py
+++ app/connection_web.py
@@ -1,14 +1,34 @@
-import requests
-import urllib.request
+import urllib
+import urllib.request as req
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 def get_pdf():
     url = "https://www.chitose.ac.jp/info/access"
     path = "//*[@id='paragraph_107_1615971519']/div/div/div[2]/a"
     savefile = "pdf/Bus.pdf"
 
-    response = urllib.request.urlopen(url).read()
-    urllib.request.urlretrieve(url,savefile)
+    #サイトと接続
+    res = req.urlopen(url)
+    soup = BeautifulSoup(res,"html.parser")
+    result = soup.select("a[href]")
 
+    #hrefタグの抽出
+    link_list = []
+    for link in result:
+        href = link.get("href")
+        link_list.append(href)
 
+    #pdfファイルの抽出
+    pdf_list = [temp for temp in link_list if temp.endswith('pdf')]
+    print(pdf_list[0])
 
-    print(response)
+    #相対パスから絶対パスに変換
+    abs_pdf_list = []
+    for relative in pdf_list:
+        temp_url = urljoin(url, relative)
+        abs_pdf_list.append(temp_url)
+    print(abs_pdf_list)
+
+    #pdfフォルダにBus.pdfとして保存
+    urllib.request.urlretrieve(abs_pdf_list[0],savefile)
